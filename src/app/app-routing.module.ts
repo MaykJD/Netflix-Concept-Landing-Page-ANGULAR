4979497c333f690529d4310b36648e9fed40774d
Mayk Jadam <maykjadam@alu.ufc.br>
2022-01-19 04:34:38 -0300

    Adeicionado as telas de cadastro de filme e cadastrar TAG Beta-0.2.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//as
+import { HomeComponent } from './components/home/home.component';
+import { CadastroCategoriaComponent } from './components/cadastro/cadastro-categoria/cadastro-categoria.component';
+import { CadastroFilmesComponent } from './components/cadastro/cadastro-filmes/cadastro-filmes.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'cadastrarFilmes', component:CadastroFilmesComponent},
+  {path: 'cadastrarCategoria', component:CadastroCategoriaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardCapaFilmeComponent } from './card-capa-filme/card-capa-filme.component';
-import { CardPosterFilmeComponent } from './card-poster-filme/card-poster-filme.component';
-import { NavFilmeComponent } from './nav-filme/nav-filme.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { NgImageSliderModule } from 'ng-image-video-gallery';
 import { EmbedVideo } from 'ngx-embed-video';import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
-import { NavFooterComponent } from './nav-footer/nav-footer.component';
 
+
+import { CardCapaFilmeComponent } from './cards/card-capa-filme/card-capa-filme.component';
+import { CardPosterFilmeComponent } from './cards/card-poster-filme/card-poster-filme.component';
+
+import { NavFilmeComponent } from './header/nav-filme/nav-filme.component';
+import { NavFooterComponent } from './header/nav-footer/nav-footer.component';
+
+import { CadastroCategoriaComponent } from './components/cadastro/cadastro-categoria/cadastro-categoria.component';
+import { CadastroFilmesComponent } from './components/cadastro/cadastro-filmes/cadastro-filmes.component';
+
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -20,7 +28,10 @@ import { NavFooterComponent } from './nav-footer/nav-footer.component';
     CardCapaFilmeComponent,
     CardPosterFilmeComponent,
     NavFilmeComponent,
-    NavFooterComponent
+    NavFooterComponent,
+    CadastroCategoriaComponent,
+    CadastroFilmesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +40,8 @@ import { NavFooterComponent } from './nav-footer/nav-footer.component';
     BrowserAnimationsModule,
     NgImageSliderModule,
     EmbedVideo,
-    NgxYoutubePlayerModule.forRoot()
+    NgxYoutubePlayerModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
